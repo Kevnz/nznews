014730cd458886dd2137742ef5519a142e8eb6ef
Kevin Isom <kevin.isom@gmail.com>
2013-11-14 16:02:06 +1300

    error handles

--- routes/stories.js
+++ routes/stories.js
@@ -19,6 +19,9 @@ exports.showStories = function(req, res){
     var stories = require('../lib/db')('NewsItem');
     var skip = req.query.start || 0;
     stories.find().sort({PublishedDate: -1}).limit(40).skip(0).toArray(function(err, docs) {
+        if (err) {
+            res.send(200, err);
+        }
         res.send(docs);
     });
 };
